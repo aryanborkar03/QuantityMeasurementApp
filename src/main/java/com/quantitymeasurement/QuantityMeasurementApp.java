6dc7ee627da096f0c03ff044b42a677adc5b6d8b
Aryan <borkararyan03@gmail.com>
2026-02-23 17:54:20 +0530

    [Aryan added : QuantityMeasurementApp]

--- src/main/java/com/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/quantitymeasurement/QuantityMeasurementApp.java
@@ -6,15 +6,23 @@ public class QuantityMeasurementApp {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		
-		// Feet Measurement
-		System.out.println("Enter First Entry (Feet) : ");
+		System.out.println("Enter First Number : ");
 		double inputOne = sc.nextDouble();
-		System.out.println("Enter Second Entry (Feet) : ");
+		System.out.println("Enter Second Number : ");
 		double inputTwo = sc.nextDouble();
 		Feet f1 = new Feet(inputOne);
         Feet f2 = new Feet(inputTwo);
+
         System.out.println("Are equal? " + f1.equals(f2));
+        
+        System.out.println("Enter First Entry (Inch) : ");
+        double inputOneInch = sc.nextDouble();
+        System.out.println("Enter Second Entry (Inch) : ");
+        double inputTwoInch = sc.nextDouble();
+        Inches i1 = new Inches(inputOneInch);
+        Inches i2 = new Inches(inputTwoInch);
+        
+        System.out.println("Are equal? " + i1.equals(i2));
         sc.close();
 	}
 }
